e67aea51cd59f58f4c48f36cf3969c82f5f53f5f
bambismurfen <bambismurfen@gmail.com>
2016-08-27 14:34:35 +1000

    We were supposed to use MvvmCross for our project so i deleted the old project and created a new Project with Mvvm with a "Hello World" Android app. I tried starting it so you should be able to emulate it.

---
+++ CatchUp/CatchUp.Core/MyClass.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace CatchUp.Core
+{
+	public class MyClass
+	{
+		public MyClass()
+		{
+		}
+	}
+}
+
---
+++ CatchUp/CatchUp.droid/obj/Debug/android/src/md572908283645bd738c6931db4169c60a0/SplashScreen.java
@@ -0,0 +1,38 @@
+package md572908283645bd738c6931db4169c60a0;
+
+
+public class SplashScreen
+	extends md5c293e307133ee8f46151deed2480c6a8.MvxSplashScreenActivity
+	implements
+		mono.android.IGCUserPeer
+{
+/** @hide */
+	public static final String __md_methods;
+	static {
+		__md_methods = 
+			"";
+		mono.android.Runtime.register ("CatchUp.droid.SplashScreen, CatchUp.droid, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", SplashScreen.class, __md_methods);
+	}
+
+
+	public SplashScreen () throws java.lang.Throwable
+	{
+		super ();
+		if (getClass () == SplashScreen.class)
+			mono.android.TypeManager.Activate ("CatchUp.droid.SplashScreen, CatchUp.droid, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", "", this, new java.lang.Object[] {  });
+	}
+
+	private java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
